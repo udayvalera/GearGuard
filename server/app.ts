bfa5627b9b39c0bc661972f8c965090ef290298d
Alfez Tintoiya <alfez.tintoiya25@gmail.com>
2025-12-27 15:34:49 +0530

    Bug Fix

--- server/app.ts
+++ server/app.ts
@@ -25,7 +25,7 @@ const apiVersion = '/api/v1';
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 app.use(helmet());
